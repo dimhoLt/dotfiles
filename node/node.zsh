3c43042db752cd419b94b29381575c6979d82f81
Patrick Strang <strangnet@gmail.com>
2012-10-04 16:45:29 +0200

    additional directory to look for node modules. a change in node 0.8 seems to be the reason, where npm is included in node.

--- node/node.zsh
+++ node/node.zsh
@@ -1,3 +1,3 @@
-export NODE_PATH=/usr/local/lib/node_modules
+export NODE_PATH=/usr/local/lib/node_modules:/usr/local/share/npm/lib/node_modules
 export PATH=$PATH:$NODE_PATH
 alias install-npm='curl http://npmjs.org/install.sh | sh'
